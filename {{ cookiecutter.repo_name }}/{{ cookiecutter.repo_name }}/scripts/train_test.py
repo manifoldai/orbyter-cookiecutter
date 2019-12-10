a90065fce50c3e50b609f584e6962202814ab9b9
Sourav Dey <sdey@manifold.ai>
2019-12-09 16:38:58 -0800

    Making config file path in tests not absolute

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/scripts/train_test.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/scripts/train_test.py
@@ -4,7 +4,7 @@ from click.testing import CliRunner
 from {{cookiecutter.repo_name}}.scripts.train import main
 
 
-@pytest.mark.parametrize("config_file", [("/mnt/configs/config.yml")])
+@pytest.mark.parametrize("config_file", [("configs/config.yml")])
 def test_train(config_file):
     runner = CliRunner()
     result = runner.invoke(main, [config_file])
