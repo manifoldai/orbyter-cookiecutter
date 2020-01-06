b42bc321989b9345949a647c7358cb8be186f2f1
Michael Stefferson <mstefferson@gmail.com>
2020-01-06 12:49:34 -0800

    Jinja replace dashes to underscore

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name.replace("-", "_") }}/scripts/evaluate_test.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name.replace("-", "_") }}/scripts/evaluate_test.py
@@ -1,11 +1,11 @@
 import pytest
 from click.testing import CliRunner
 
-from {{cookiecutter.repo_name}}.scripts.train import main
+from {{cookiecutter.repo_name.replace("-", "_")}}.scripts.evaluate import main
 
 
 @pytest.mark.parametrize("config_file", [("configs/config.yml")])
-def test_train(config_file):
+def test_evaluate(config_file):
     runner = CliRunner()
     result = runner.invoke(main, [config_file])
     assert result.exit_code == 0
