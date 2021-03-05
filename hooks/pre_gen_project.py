9a80997dc1e37d57102059c6bea7e197bebc6a37
Ramesh Sridharan <rsridharan@manifold.ai>
2021-03-05 14:44:07 -0800

    fix(validation): make validation hook python2 compatible

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,10 +1,16 @@
-"""Python module name validation."""
+"""Python module name validation.
+
+Compatible with python 2 and python 3, to ensure cookiecutter support
+across various platforms
+"""
 import logging
 import sys
 import re
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
+
+REF_URL = "https://www.python.org/dev/peps/pep-0008/#package-and-module-names"
 
 
 def validate_python_package_name():
@@ -13,11 +19,8 @@ def validate_python_package_name():
     # The following regex will only match valid Python module names
     if not re.match(r"^[a-z][_a-z0-9]+$", package_name):
         # Error out if you have an invalid module name
-        logger.error(
-            f"{package_name} is not a valid Python module name!\n See "
-            "https://www.python.org/dev/peps/pep-0008/#package-and-module-names "
-            "for naming standards.\n"
-        )
+        err = "{} is not a valid Python module name!\n See {} for naming standards."
+        logger.error(err.format(package_name, REF_URL))
         sys.exit(1)
 
 
